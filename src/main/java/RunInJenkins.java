f54e4f520c206a1aa1a2692636a6677fe98fc1b1
kprasadarao@yahoo.com <kprasadarao@yahoo.com>
2017-05-25 12:33:08 +0100

    adding testng demo from local

---
+++ src/main/java/RunInJenkins.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class RunInJenkins{
+	
+	
+	@Test
+	public void displayTest(){
+		System.out.println("**********this is test file from maven to jenkins*********** ");
+	}
+
+}
